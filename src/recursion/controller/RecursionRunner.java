d90bfa88d6e82bbfc72d492aaee972ed97c56e5a
ZacEaster <easter.zac@gmail.com>
2014-01-22 11:28:43 -0700

    Commit

---
+++ src/recursion/controller/RecursionRunner.java
@@ -0,0 +1,16 @@
+package recursion.controller;
+
+public class RecursionRunner
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		FibRecursion myFibRecursion = new FibRecursion();
+		myFibRecursion.start();
+
+	}
+
+}
